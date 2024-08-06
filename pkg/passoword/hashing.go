f795b0eb8bdec49cbf797941a0b2a8adfc754ecb
Amjad Jibon <amjad.jibon@gmail.com>
2024-08-06 09:01:20 +0800

    implement password hash

---
+++ pkg/passoword/hashing.go
@@ -0,0 +1,14 @@
+package passoword
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPassword(password, hash string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+}
